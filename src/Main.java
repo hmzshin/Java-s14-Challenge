16d0a6707433802e4a24dd1ff2c503f6f7a3e728
hmzshin <hamzasahin@outlook.com>
2024-01-19 21:44:20 +0300

    Görev tanımında verilen testleri tanımlar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hamburger hamburger = new Hamburger("Basic", "beef", 3.56, "Wrap");
+
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+
+        hamburger.itemizeHamburger();
+
+        HealthyHamburger healthyBurger = new HealthyHamburger("Vegan Burger", "Chicken", 5.67, "Sandwich");
+
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+
+        healthyBurger.addHealthyAddition1("Lentils", 3.41);
+
+        healthyBurger.itemizeHamburger();
+
+        DeluxeBurger db = new DeluxeBurger();
+
+        db.addHamburgerAddition3("Should not do this", 50.53);
+
+        db.itemizeHamburger();
     }
 }
